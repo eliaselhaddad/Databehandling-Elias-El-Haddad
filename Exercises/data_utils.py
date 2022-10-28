b1ae455c2b8cb2f86ce437f1f46fdb1ca7c19ccf
eliaselhaddad <86868035+eliaselhaddad@users.noreply.github.com>
2022-10-28 17:26:03 +0200

    test

---
+++ Exercises/data_utils.py
@@ -0,0 +1,19 @@
+# Create a function that takes in a DataFrame as input parameter and plots a barplot with the columns that have missing values
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_missing_values(df):
+    # Create a list of columns that have missing values
+    cols_with_missing = [col for col in df.columns if df[col].isnull().any()] # any() returns True if any of the values in the column is missing
+    # Create a bar plot of the columns that have missing values
+    sns.barplot(x=cols_with_missing, y=df[cols_with_missing].isnull().sum())
+    plt.xticks(rotation=90)
+    plt.show()
+
+
+
+
+
+
+
