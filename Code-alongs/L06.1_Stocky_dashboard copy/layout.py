5e77816f5013a89f7ff192c792b129ad70b65ac3
eliaselhaddad <86868035+eliaselhaddad@users.noreply.github.com>
2022-11-15 10:20:31 +0100

    stocky dashboard

--- Code-alongs/L06.1_Stocky_dashboard copy/layout.py
+++ Code-alongs/L06.1_Stocky_dashboard copy/layout.py
@@ -34,7 +34,10 @@ class Layout:
                         dbc.Col(
                             html.P("Choose a stock"),
                             className="mt-1",
-                            lg="4",
+                            xs=12,
+                            sm=12,
+                            md=6,
+                            lg=4,
                             xl={"offset": 2, "size": 2},
                         ),
                         dbc.Col(
@@ -43,8 +46,12 @@ class Layout:
                                 options=self._stock_options_dropdown,
                                 value="AAPL",
                             ),
-                            lg="4",
-                            xl="3",
+                            xs=12,
+                            sm=12,
+                            md=12,
+                            lg=4,
+                            xl=3,
+
                         ),
                         dbc.Col(
                             dbc.Card(
@@ -55,8 +62,11 @@ class Layout:
                                     value="close",
                                 )
                             ),
-                            lg="4",
-                            xl="3",
+                            xs=12,
+                            sm=12,
+                            md=12,
+                            lg=4,
+                            xl=3,
                         ),
                     ],
                 ),
@@ -74,6 +84,9 @@ class Layout:
                                     step=None,
                                 ),
                             ],
+                            xs=12,
+                            sm=12,
+                            md=12,
                             lg={"size": 6},
                             xl=6,
                         ),
@@ -82,20 +95,36 @@ class Layout:
                                 dbc.Row(
                                     dbc.Card(
                                         [
-                                            html.H2("Highest value"),
-                                            html.P(id="highest-value"),
+                                            html.H2(
+                                                "Highest value",
+                                                className="h5 mt-3 mx-3",
+                                            ),
+                                            html.P(
+                                                id="highest-value",
+                                                className="h1 mx-2 text-success",
+                                            ),
                                         ]
-                                    )
+                                    ),
+                                    className="mt-5 h-25",
                                 ),
                                 dbc.Row(
                                     dbc.Card(
                                         [
-                                            html.H2("Lowest value"),
-                                            html.P(id="lowest-value"),
+                                            html.H2(
+                                                "Lowest value", className="h5 mt-3 mx-3"
+                                            ),
+                                            html.P(
+                                                id="lowest-value",
+                                                className="h1 mx-2 text-danger",
+                                            ),
                                         ]
-                                    )
+                                    ),
+                                    className="mt-5 h-25",
                                 ),
-                            ]
+                            ],
+                            lg=3,
+                            xl=2,
+                            class_name="mt-5 mx-5",
                         ),
                     ]
                 ),
